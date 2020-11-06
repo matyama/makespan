7d5ad6c9fe96334c336d640bfaa59bedca8d38f3
Martin Matyasek <martin.matyasek@gmail.com>
2020-11-06 12:13:16 +0100

    Add more LPT benchmarks

--- benches/scheduler.rs
+++ benches/scheduler.rs
@@ -13,6 +13,19 @@ const PROCESSING_TIMES: [f64; 7] = [5., 5., 4., 4., 3., 3., 3.];
 const IDENTICAL_TIMES: [f64; 7] = [1.; 7];
 const IDENTICAL_TIMES_LONG: [f64; 50] = [1.; 50];
 
+// TODO: should rather return `Vec<usize>`
+fn subopt_instance(num_resources: usize) -> Vec<f64> {
+    let mut pts = vec![0usize; 2 * num_resources + 1];
+    let mut pt = 2 * num_resources - 1;
+    while pt >= num_resources {
+        pts.push(pt);
+        pts.push(pt);
+        pt -= 1;
+    }
+    pts.push(num_resources);
+    pts.into_iter().map(|pt| pt as f64).collect()
+}
+
 // TODO: Deduplicate with macro
 
 #[bench]
@@ -69,4 +82,25 @@ fn bnb_3_resources_50_identical_tasks(b: &mut Bencher) {
 fn bnb_3_resources_50_identical_tasks_longer(b: &mut Bencher) {
     let pts = vec![10.; 50];
     b.iter(|| BNB.schedule(&pts, 3));
+}
+
+#[bench]
+fn lpt_subopt_10(b: &mut Bencher) {
+    let num_resources = 10;
+    let processing_times = subopt_instance(num_resources);
+    b.iter(|| LPT.schedule(&processing_times, num_resources));
+}
+
+#[bench]
+fn lpt_subopt_100(b: &mut Bencher) {
+    let num_resources = 100;
+    let processing_times = subopt_instance(num_resources);
+    b.iter(|| LPT.schedule(&processing_times, num_resources));
+}
+
+#[bench]
+fn lpt_subopt_1000(b: &mut Bencher) {
+    let num_resources = 1000;
+    let processing_times = subopt_instance(num_resources);
+    b.iter(|| LPT.schedule(&processing_times, num_resources));
 }
